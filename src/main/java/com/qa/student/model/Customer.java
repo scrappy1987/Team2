85eebdead5d013d1119e31010a3c6a217f3fd599
Jai Kanabar <jaikanabar@hotmail.com>
2017-01-30 16:31:15 +0000

    QC-55-Create-Customer-Entity

---
+++ src/main/java/com/qa/student/model/Customer.java
@@ -0,0 +1,118 @@
+package com.qa.student.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+
+@Entity
+public class Customer {
+
+	public Customer(String firstName, String surname, String email, int phone,
+			String dateOfBirth, String password, String accountType) {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Id
+	@GeneratedValue
+	private int customer_ID;
+
+	private int address_ID;
+	
+	@Column(name = "first_name")
+	@NotNull
+	private String firstName;
+	
+	@NotNull
+	private String surname;
+	
+	@NotNull
+	private String email;
+	
+	private int phone;
+	
+	@Column(name = "date_of_birth")
+	private String dateOfBirth;
+	
+	private String password;
+	
+	@Column(name = "account_type")
+	@NotNull
+	private String accountType;
+	
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getPhone() {
+		return phone;
+	}
+
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getAccountType() {
+		return accountType;
+	}
+
+	public void setAccountType(String accountType) {
+		this.accountType = accountType;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String newFirstName) {
+		this.firstName = newFirstName;
+		
+	}
+	
+	public int getAddress_ID() {
+		return address_ID;
+	}
+
+	public void setAddress_ID(int address_ID) {
+		this.address_ID = address_ID;
+	}
+
+	public int getCustomer_ID() {
+		return customer_ID;
+	}
+
+	public void setCustomer_ID(int customer_ID) {
+		this.customer_ID = customer_ID;
+	}
+
+}
---
+++ src/test/java/com/qa/student/model/CustomerTest.java
@@ -0,0 +1,115 @@
+package com.qa.student.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CustomerTest {
+	
+	
+	
+	@Test
+	public void testGetFirstName() {
+		Customer testCustomer = new Customer("Coolin", "McCool", "Cmail@mail.com", 071231231, "13/11/55", "password", "Basic");
+		String name = testCustomer.getFirstName();
+		assertEquals(name, "Coolin");
+	}
+	
+	@Test
+	public void testSetFirstName() {
+		Customer testCustomer = new Customer("Coolin", "McCool", "Cmail@mail.com", 071231231, "13/11/55", "password", "Basic");
+		testCustomer.setFirstName("Moolin");
+		String name = testCustomer.getFirstName();
+		assertEquals(name, "Moolin");
+	}
+	
+	@Test
+	public void testGetSurname() {
+		Customer testCustomer = new Customer("Coolin", "McCool", "Cmail@mail.com", 071231231, "13/11/55", "password", "Basic");
+		String name = testCustomer.getSurname();
+		assertEquals(name, "McCool");
+	}	
+	
+	@Test
+	public void testSetSurname() {
+		Customer testCustomer = new Customer("Coolin", "McCool", "Cmail@mail.com", 071231231, "13/11/55", "password", "Basic");
+		testCustomer.setSurname("McMool");
+		String name = testCustomer.getSurname();
+		assertEquals(name, "McMool");
+	}
+	
+	@Test
+	public void testGetEmail() {
+		Customer testCustomer = new Customer("Coolin", "McCool", "Cmail@mail.com", 071231231, "13/11/55", "password", "Basic");
+		String email = testCustomer.getEmail();
+		assertEquals(email, "Cmail@mail.com");
+	}
+	
+	@Test
+	public void testSetEmail() {
+		Customer testCustomer = new Customer("Coolin", "McCool", "Cmail@mail.com", 071231231, "13/11/55", "password", "Basic");
+		testCustomer.setSurname("Coolin@mail.com");
+		String email = testCustomer.getEmail();
+		assertEquals(email, "Coolin@mail.com");
+	}
+
+	@Test
+	public void testGetPhone() {
+		Customer testCustomer = new Customer("Coolin", "McCool", "Cmail@mail.com", 071231231, "13/11/55", "password", "Basic");
+		int phone = testCustomer.getPhone();
+		assertEquals(phone, 071231231);
+	}
+	
+	@Test
+	public void testSetPhone() {
+		Customer testCustomer = new Customer("Coolin", "McCool", "Cmail@mail.com", 071231231, "13/11/55", "password", "Basic");
+		testCustomer.setPhone(073213213);
+		int phone = testCustomer.getPhone();
+		assertEquals(phone, 073213213);
+	}
+	
+	@Test
+	public void testGetDateOfBirth() {
+		Customer testCustomer = new Customer("Coolin", "McCool", "Cmail@mail.com", 071231231, "13/11/55", "password", "Basic");
+		String dob = testCustomer.getDateOfBirth();
+		assertEquals(dob, "13/11/55");
+	}
+	
+	@Test
+	public void testSetDateofBirth() {
+		Customer testCustomer = new Customer("Coolin", "McCool", "Cmail@mail.com", 071231231, "13/11/55", "password", "Basic");
+		testCustomer.setDateOfBirth("01/01/92");
+		String dob = testCustomer.getDateOfBirth();
+		assertEquals(dob, "01/01/92");
+	}
+	
+	@Test
+	public void testGetPassword() {
+		Customer testCustomer = new Customer("Coolin", "McCool", "Cmail@mail.com", 071231231, "13/11/55", "password", "Basic");
+		String pword = testCustomer.getPassword();
+		assertEquals(pword, "password");
+	}
+	
+	@Test
+	public void testSetPassword() {
+		Customer testCustomer = new Customer("Coolin", "McCool", "Cmail@mail.com", 071231231, "13/11/55", "password", "Basic");
+		testCustomer.setPassword("drowssap");
+		String pword = testCustomer.getPassword();
+		assertEquals(pword, "drowssap");
+	}
+	
+	@Test
+	public void testGetAccountType() {
+		Customer testCustomer = new Customer("Coolin", "McCool", "Cmail@mail.com", 071231231, "13/11/55", "password", "Basic");
+		String aType = testCustomer.getAccountType();
+		assertEquals(aType, "basic");
+	}
+	
+	@Test
+	public void testSetAccountType() {
+		Customer testCustomer = new Customer("Coolin", "McCool", "Cmail@mail.com", 071231231, "13/11/55", "password", "Basic");
+		testCustomer.setAccountType("Premium");
+		String aType = testCustomer.getAccountType();
+		assertEquals(aType, "Premium");
+	}
+}
