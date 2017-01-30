69981307701a6e1907b79b187eaeb92299820ae2
Administrator <Administrator@STUDENT09>
2017-01-30 15:58:50 +0000

    Address and address Test

---
+++ src/main/java/com/qa/student/model/Address.java
@@ -0,0 +1,62 @@
+package com.qa.student.model;
+
+
+public class Address {
+	//attributes
+	private String FirstLine;
+	private String SecondLine;
+	private String TownCity;
+	private String County;
+	private String Postcode;
+	
+	//Constructors
+	public Address(){
+		
+	}
+	public Address(String firstLine, String secondLine, String townCity, String county, String postcode) {
+		FirstLine = firstLine;
+		SecondLine = secondLine;
+		TownCity = townCity;
+		County = county;
+		Postcode = postcode;
+	}
+	public Address(String firstLine, String postcode) {
+		FirstLine = firstLine;
+		Postcode = postcode;
+	}
+	
+	//methods
+	public String getFirstLine() {
+		return FirstLine;
+	}
+	public void setFirstLine(String firstLine) {
+		FirstLine = firstLine;
+	}
+	public String getSecondLine() {
+		return SecondLine;
+	}
+	public void setSecondLine(String secondLine) {
+		SecondLine = secondLine;
+	}
+	public String getTownCity() {
+		return TownCity;
+	}
+	public void setTownCity(String townCity) {
+		TownCity = townCity;
+	}
+	public String getCounty() {
+		return County;
+	}
+	public void setCounty(String county) {
+		County = county;
+	}
+	public String getPostcode() {
+		return Postcode;
+	}
+	public void setPostcode(String postcode) {
+		Postcode = postcode;
+	}
+	
+	
+
+}
