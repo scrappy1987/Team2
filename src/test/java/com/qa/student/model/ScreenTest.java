2ee102d6f0ea22e7d1e4f3737d0cbca62e0f96c1
fwasim <farhaad.wasim@googlemail.com>
2017-01-30 16:28:07 +0000

    Created Screen entity and test

---
+++ src/test/java/com/qa/student/model/ScreenTest.java
@@ -0,0 +1,42 @@
+package com.qa.student.model;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.qa.student.model.Screen.ShowingTypes;
+
+/**
+ * 
+ * @author FWasim
+ *
+ */
+public class ScreenTest {
+
+	@Test
+	public void testScreenConstructor() {
+		Screen screen = new Screen(1L, 25, 30, ShowingTypes.STANDARD_3D);
+		Assert.assertNotNull(screen);
+	}
+
+	@Test
+	public void testGetShowingTypes() {
+		Screen screen = new Screen();
+		screen.setShowingTypes(ShowingTypes.STANDARD_2D);
+		Assert.assertEquals(ShowingTypes.STANDARD_2D, screen.getShowingTypes());
+	}
+
+	@Test
+	public void testGetNumberOfRegularSeats() {
+		Screen screen = new Screen();
+		screen.setNoOfRegularSeats(25);
+		Assert.assertEquals(25, screen.getNoOfRegularSeats());
+	}
+
+	@Test
+	public void testGetNumberOfPremiumSeats() {
+		Screen screen = new Screen();
+		screen.setNoOfPremiumSeats(30);
+		Assert.assertEquals(30, screen.getNoOfPremiumSeats());
+	}
+
+}
