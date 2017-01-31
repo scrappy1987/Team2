1e8f46b1702ebe7225a838e7b5df280a8ab80ed3
lameay <leimeih@gmail.com>
2017-01-31 16:36:19 +0000

    Movie Service Test created, Movie Service class created

---
+++ src/test/java/com/qa/student/rest/MovieServiceTest.java
@@ -0,0 +1,25 @@
+package com.qa.student.rest;
+
+import static org.junit.Assert.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import com.qa.student.model.Movie;
+
+public class MovieServiceTest {
+
+	@Test
+	public void movieServiceTest() {
+		
+		MovieService testMovieService = new MovieService();
+		fail("Not yet implemented");
+	}
+
+}
