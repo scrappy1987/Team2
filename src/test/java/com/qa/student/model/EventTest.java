bd66014e6e1fe083e5e869a79639c0710560ae90
Jai Kanabar <jaikanabar@hotmail.com>
2017-02-01 12:00:40 +0000

    QC-87-Create-Branch-on-Github-to-Merge-Entities
    Added venue, cleaned up some bits of code, Showing tests need fixing.

--- src/test/java/com/qa/student/model/EventTest.java
+++ src/test/java/com/qa/student/model/EventTest.java
@@ -4,24 +4,26 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.qa.student.service.entities.Event;
+
 public class EventTest {
 
 	@Test
 	public void testEvent1() {
-		event eve = new event("the life of trev", "what is trev up to", "17.38");
+		Event eve = new Event("the life of trev", "what is trev up to", "17.38");
 		eve.setTitle("the life of john");
 		assertEquals("the life of john", eve.getTitle());
 	}
 	
 	@Test
 	public void testEvent2() {
-		event eve = new event("the life of trev", "what is trev up to", "17.38");
+		Event eve = new Event("the life of trev", "what is trev up to", "17.38");
 		eve.setDescription("what is john up to");
 		assertEquals("what is john up to", eve.getDescription());
 	}
 	@Test
 	public void testEvent3() {
-		event eve = new event("the life of trev", "what is trev up to", "17.01");
+		Event eve = new Event("the life of trev", "what is trev up to", "17.01");
 		eve.setTitle("17.00");
 		assertEquals("17.01", eve.getTitle());
 	}
