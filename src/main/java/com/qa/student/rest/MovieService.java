4e375b5332f34c7621034f9447f4c8c900de75c5
lameay <leimeih@gmail.com>
2017-02-03 09:56:59 +0000

    Commit before installing new Eclipse

--- src/main/java/com/qa/student/rest/MovieService.java
+++ src/main/java/com/qa/student/rest/MovieService.java
@@ -1,5 +1,59 @@
 package com.qa.student.rest;
 
-public class MovieService {
+import java.util.ArrayList;
+import java.util.List;
 
+import com.google.gson.*;
+import com.qa.student.model.Movie;
+import com.qa.student.model.Venue;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/movies")
+@RequestScoped
+public class MovieService {
+	
+	@Inject
+	EntityManager em;
+	
+	@GET
+	@Path("/allmoviesjson")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getAllMoviesAsJSON() {
+		List<Movie> results = getAllMovies();
+		Gson toJson = new Gson();
+		String result = toJson.toJson(results, Movie.class);
+		return result;
+		
+	}
+
+	@GET
+	@Path("/{movieSearchTerm}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getMoviesByTitleAsJSON(@PathParam("movieSearchTerm") String movieSearchTerm) {
+		
+		List<Movie> results = getMoviesByTitle(movieSearchTerm);
+		Gson toJson = new Gson();
+		String result = toJson.toJson(results, Movie.class);
+		return result;
+		
+	}
+
+	public List<Movie> getAllMovies() {
+		final List<Movie> results = em.createQuery("select title from Movies order by title", Movie.class).getResultList();
+		return results;
+	}
+	
+	public List<Movie> getMoviesByTitle(String movieSearchTerm) {
+		final List<Movie> results = em.createQuery("select title from Movies where title = '" + movieSearchTerm + "", Movie.class).getResultList();
+		return results;
+	}
+	
 }
--- src/test/java/com/qa/student/rest/MovieServiceTest.java
+++ src/test/java/com/qa/student/rest/MovieServiceTest.java
@@ -2,8 +2,12 @@ package com.qa.student.rest;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,15 +15,47 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
+
 import com.qa.student.model.Movie;
 
+@RunWith(MockitoJUnitRunner.class)
 public class MovieServiceTest {
+	
+	@InjectMocks
+	MovieService testMovieService = new MovieService();
+	
+	@Mock
+	EntityManager em;
+	
+	@Mock
+	Query query;
 
 	@Test
-	public void movieServiceTest() {
+	public void getAllMoviesPOJOTest() {
 		
-		MovieService testMovieService = new MovieService();
-		fail("Not yet implemented");
+		List<Movie> movieList = new ArrayList<Movie>();
+		
+		Movie newMovie = new Movie();
+		newMovie.setTitle("Harry Potter and the Philosopher's Stone");
+		movieList.add(newMovie);
+		
+		Mockito.when(em.createQuery(Mockito.anyString())).thenReturn(query);
+		Mockito.when(query.getResultList()).thenReturn(movieList);
+		
+		List<Movie> assertionList = testMovieService.getAllMovies();
+		Assert.assertEquals(assertionList.get(0).getTitle(), "Harry Potter and the Philosopher's Stone");
+		
+		Mockito.verify(em).createQuery(Mockito.anyString());
+		
 	}
+	
+	@Test
+	public void getAllMoviesJSONTest() {
+		
+		String result = testMovieService.getAllMoviesAsJSON();
+		
+		
+	}
+	
 
 }
